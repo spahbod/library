02eecaf774519a47219ab80684f6900032155b50
spahbod@gmail.com <87122902474Ab!>
2023-12-01 14:42:43 +0100

    Poprawki sonar.

--- src/main/java/my/home/library/controller/BookController.java
+++ src/main/java/my/home/library/controller/BookController.java
@@ -1,9 +1,9 @@
 package my.home.library.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import my.home.library.model.Book;
 import my.home.library.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Slf4j
+@AllArgsConstructor
 @Controller
 public class BookController {
-    @Autowired
-    private BookService bookService;
+
+    private final BookService bookService;
 
     @GetMapping("/")
     public String getAllBooks(Model model) {
--- src/main/java/my/home/library/service/BookService.java
+++ src/main/java/my/home/library/service/BookService.java
@@ -1,16 +1,17 @@
 package my.home.library.service;
 
+import lombok.AllArgsConstructor;
 import my.home.library.model.Book;
 import my.home.library.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class BookService {
-    @Autowired
-    private BookRepository bookRepository;
+
+    private final BookRepository bookRepository;
 
     public List<Book> list() {
         return bookRepository.findAll();
